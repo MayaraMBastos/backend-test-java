2d1cebe9dea7210b227d1f576389031bcaf9dbc0
MayaraMB <maybastos2021@gmail.com>
2025-05-22 13:35:25 -0300

    feat: add atributos da entidade EmpresaEntity (id, nome, cnpj e endereco). Obs: possibilidade de add entidade para o endereco e trabalhar com relacao one to many

--- src/main/java/com/meudroz/backend_test_java/Model/EmpresaEntity.java
+++ src/main/java/com/meudroz/backend_test_java/Model/EmpresaEntity.java
@@ -1,9 +1,61 @@
 package com.meudroz.backend_test_java.Model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "empresas")
 public class EmpresaEntity {
 
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    @NotBlank
+    @NotNull
+    private String nome;
+    @Column(name = "cnpj",  unique = true)
+    @NotNull
+    @NotBlank
+    private String cnpj;
+    @Column(name = "endereco")
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL)
+    private String endereco;
 
+//    @OneToMany(mappedBy = "empresas", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Endereco> enderecos = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
