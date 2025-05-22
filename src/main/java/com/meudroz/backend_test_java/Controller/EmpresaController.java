d5b7086295d5deaa90471b701e7842f275309839
MayaraMB <maybastos2021@gmail.com>
2025-05-22 17:04:52 -0300

    refactor: atribuidas resposabilidades de salvar e atualizar dados ao service, modificados osmetodos post e put do controller

--- src/main/java/com/meudroz/backend_test_java/Controller/EmpresaController.java
+++ src/main/java/com/meudroz/backend_test_java/Controller/EmpresaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.meudroz.backend_test_java.EmpresaDTO.EmpresaDTO;
+import com.meudroz.backend_test_java.Service.EmpresaService;
 import com.meudroz.backend_test_java.Utils.EmpresaValidator;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,125 +29,115 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Empresas", description = "Endpoints para cadastro e consulta de empresas")
 public class EmpresaController {
 
-  private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-  private EmpresaValidator empresaValidator = new EmpresaValidator();
+    private EmpresaValidator empresaValidator = new EmpresaValidator();
 
-  public EmpresaController(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
-  }
+    private EmpresaService empresaService;
 
-private EmpresaDTO empresa;
+    public EmpresaController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-  @Operation(summary = "Listar todas as empresas")
-  @ApiResponse(responseCode = "200", description = "Lista de empresas cadastradas", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(example = """
-        {
-          "nome": "JAVA TESTE Ltda",
-          "cnpj": "12.345.678/0001-12",
-          "endereco": "Rua do teste, 123"
+    private EmpresaDTO empresa;
+
+    @Operation(summary = "Listar todas as empresas")
+    @ApiResponse(responseCode = "200", description = "Lista de empresas cadastradas", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(example = """
+              {
+                "nome": "JAVA TESTE Ltda",
+                "cnpj": "12.345.678/0001-12",
+                "endereco": "Rua do teste, 123"
+              }
+            """))))
+    @GetMapping(produces = "application/json")
+    public List<Map<String, Object>> listarEmpresas() {
+        String sql = "SELECT nome, cnpj, endereco FROM empresas";
+        List<Map<String, Object>> empresas = jdbcTemplate.queryForList(sql);
+
+        for (Map<String, Object> empresa : empresas) {
+            String cnpj = (String) empresa.get("cnpj");
+            empresa.put("cnpj", cnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5"));
         }
-      """))))
-  @GetMapping(produces = "application/json")
-  public List<Map<String, Object>> listarEmpresas() {
-    String sql = "SELECT nome, cnpj, endereco FROM empresas";
-    List<Map<String, Object>> empresas = jdbcTemplate.queryForList(sql);
 
-    for (Map<String, Object> empresa : empresas) {
-      String cnpj = (String) empresa.get("cnpj");
-      empresa.put("cnpj", cnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5"));
+        return empresas;
     }
 
-    return empresas;
-  }
+    @Operation(summary = "Buscar uma empresa pelo CNPJ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Empresa encontrada ou não encontrada", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                      {
+                        "nome": "JAVA TESTE Ltda",
+                        "cnpj": "12.345.678/0001-12",
+                        "endereco": "Rua do teste, 123"
+                      }
+                    """)))
+    })
+    @GetMapping(value = "/{cnpj}", produces = "application/json")
+    public Object buscarPorCnpj(@PathVariable String cnpj) {
+        String sql = "SELECT nome, cnpj, endereco FROM empresas WHERE cnpj = ?";
+        List<Map<String, Object>> resultado = jdbcTemplate.queryForList(sql, cnpj);
 
-  @Operation(summary = "Buscar uma empresa pelo CNPJ")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Empresa encontrada ou não encontrada", content = @Content(mediaType = "application/json", schema = @Schema(example = """
-            {
-              "nome": "JAVA TESTE Ltda",
-              "cnpj": "12.345.678/0001-12",
-              "endereco": "Rua do teste, 123"
-            }
-          """)))
-  })
-  @GetMapping(value = "/{cnpj}", produces = "application/json")
-  public Object buscarPorCnpj(@PathVariable String cnpj) {
-    String sql = "SELECT nome, cnpj, endereco FROM empresas WHERE cnpj = ?";
-    List<Map<String, Object>> resultado = jdbcTemplate.queryForList(sql, cnpj);
+        if (resultado.isEmpty()) {
+            return Map.of("erro", "Empresa não encontrada com o CNPJ fornecido.");
+        }
 
-    if (resultado.isEmpty()) {
-      return Map.of("erro", "Empresa não encontrada com o CNPJ fornecido.");
+        Map<String, Object> empresa = resultado.get(0);
+        String cnpjFormatado = ((String) empresa.get("cnpj")).replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})",
+                "$1.$2.$3/$4-$5");
+        empresa.put("cnpj", cnpjFormatado);
+
+        return empresa;
     }
 
-    Map<String, Object> empresa = resultado.get(0);
-    String cnpjFormatado = ((String) empresa.get("cnpj")).replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})",
-        "$1.$2.$3/$4-$5");
-    empresa.put("cnpj", cnpjFormatado);
+    @Operation(summary = "Cadastrar uma nova empresa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Empresa cadastrada ou erro de validação", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                      {
+                        "mensagem": "Empresa cadastrada com sucesso.",
+                        "linhasAfetadas": 1
+                      }
+                    """)))
+    })
 
-    return empresa;
-  }
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Map<String, Object> cadastrarEmpresa(@RequestBody EmpresaDTO empresa) {
 
-  @Operation(summary = "Cadastrar uma nova empresa")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Empresa cadastrada ou erro de validação", content = @Content(mediaType = "application/json", schema = @Schema(example = """
-            {
-              "mensagem": "Empresa cadastrada com sucesso.",
-              "linhasAfetadas": 1
-            }
-          """)))
-  })
-  @PostMapping(consumes = "application/json", produces = "application/json")
-  public Map<String, Object> cadastrarEmpresa(@RequestBody EmpresaDTO empresa) {
+        Map<String, Object> response = new HashMap<>();
 
-    Map<String, Object> response = new HashMap<>();
+        // Validação por EmpresaValidator
+        Map<String, Object> erros = empresaValidator.validarCadastroDeEmpresa(empresa);
+        if (!erros.isEmpty()) {
+            response.put("erros", erros);
+            return response;
 
-    // Validação por EmpresaValidator
-    Map<String, Object> erros = empresaValidator.validarCadastroDeEmpresa(empresa);
-    if (!erros.isEmpty()) {
-      response.put("erros", erros);
-      return response;
-
-    } else {
-
-      String sql = "INSERT INTO empresas (nome, cnpj, endereco) VALUES (?, ?, ?)";
-      int rows = jdbcTemplate.update(sql, empresa.nome, empresa.cnpj, empresa.endereco);
-      response.put("mensagem", "Empresa cadastrada com sucesso.");
-      response.put("linhasAfetadas", rows);
+        } else {
+            Map<String, Object> sucesso = empresaService.cadastrarEmpresa(empresa);
+            return sucesso;
+        }
     }
-    return response;
-  }
 
-  @Operation(summary = "Atualizar dados de uma empresa pelo CNPJ")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Empresa atualizada ou erro de validação", content = @Content(mediaType = "application/json", schema = @Schema(example = """
-            {
-              "mensagem": "Empresa atualizada com sucesso.",
-              "linhasAfetadas": 1
-            }
-          """)))
-  })
-  @PutMapping(value = "/{cnpj}", consumes = "application/json", produces = "application/json")
-  public Map<String, Object> atualizarEmpresa(@PathVariable String cnpj, @RequestBody EmpresaDTO empresa) {
-    Map<String, Object> response = new HashMap<>();
+    @Operation(summary = "Atualizar dados de uma empresa pelo CNPJ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Empresa atualizada ou erro de validação", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                      {
+                        "mensagem": "Empresa atualizada com sucesso.",
+                        "linhasAfetadas": 1
+                      }
+                    """)))
+    })
+    @PutMapping(value = "/{cnpj}", consumes = "application/json", produces = "application/json")
+    public Map<String, Object> atualizarEmpresa(@PathVariable String cnpj, @RequestBody EmpresaDTO empresa) {
+        Map<String, Object> response = new HashMap<>();
 
-    // Validação por EmpresaValidator
-    Map<String, Object> erros = empresaValidator.validarCadastroDeEmpresa(empresa);
-    if (!erros.isEmpty()) {
-      response.put("erros", erros);
-      return response;
-    } else {
+        // Validação por EmpresaValidator
+        Map<String, Object> erros = empresaValidator.validarCadastroDeEmpresa(empresa);
+        if (!erros.isEmpty()) {
+            response.put("erros", erros);
+            return response;
+        } else {
+            Map<String, Object> sucesso = empresaService.atualizarEmpresa(empresa);
+            return sucesso;
+        }
 
-      String sql = "UPDATE empresas SET nome = ?, endereco = ? WHERE cnpj = ?";
-      int rows = jdbcTemplate.update(sql, empresa.nome, empresa.endereco, cnpj);
-// a consulta deve ser feita antes de atualizar MODIFICAR
-      if (rows == 0) {
-        response.put("erro", "Nenhuma empresa encontrada com o CNPJ fornecido.");
-        return response;
-      }
-
-      response.put("mensagem", "Empresa atualizada com sucesso.");
-      response.put("linhasAfetadas", rows);
     }
-    return response;
-  }
 }
--- src/main/java/com/meudroz/backend_test_java/Service/EmpresaService.java
+++ src/main/java/com/meudroz/backend_test_java/Service/EmpresaService.java
@@ -1,7 +1,46 @@
 package com.meudroz.backend_test_java.Service;
 
+import com.meudroz.backend_test_java.EmpresaDTO.EmpresaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmpresaService {
+
+    @Autowired
+    private final JdbcTemplate jdbcTemplate;
+
+    public EmpresaService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Map<String, Object> cadastrarEmpresa(EmpresaDTO empresa){
+        Map<String, Object> response = new HashMap<>();
+
+        String sql = "INSERT INTO empresas (nome, cnpj, endereco) VALUES (?, ?, ?)";
+        int rows = jdbcTemplate.update(sql, empresa.nome, empresa.cnpj, empresa.endereco);
+        response.put("mensagem", "Empresa cadastrada com sucesso.");
+        response.put("linhasAfetadas", rows);
+        return response;
+    }
+
+    public Map<String, Object> atualizarEmpresa(EmpresaDTO empresa){
+        Map<String, Object> response = new HashMap<>();
+
+
+        String sql = "UPDATE empresas SET nome = ?, endereco = ? WHERE cnpj = ?";
+        int rows = jdbcTemplate.update(sql, empresa.nome, empresa.endereco, empresa.cnpj);
+
+        if (rows == 0) {
+            response.put("erro", "Nenhuma empresa encontrada com o CNPJ fornecido.");
+            return response;
+        }
+
+        response.put("mensagem", "Empresa atualizada com sucesso.");
+        response.put("linhasAfetadas", rows);
+    }
 }
